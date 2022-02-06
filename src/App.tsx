3bf3bc81a6c765b711e54fa3244337fb1944fc20
Robert Robson <robrobsontech@gmail.com>
2022-02-06 01:08:25 +0000

    06-02-22 - We now check to see if the input entered is the correct word for today!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Input, Layout } from "antd";
+import { Button, Input, Layout, Modal } from "antd";
 
 import GameRow from "./components/GameRow";
 import "./App.css";
@@ -10,11 +10,12 @@ function App() {
 	const [myData, setMyData] = useState<any[]>([]);
 	const [todaysPokemon, setTodaysPokemon] = useState(0);
 	const [todaysPokemonLetters, setTodaysPokemonLetters] = useState(0);
-	const [guess, setGuess] = useState("");
-	// const guesses = ["", "", "", "", ""];
 	const [guesses, setGuesses] = useState<any[]>(["", "", "", "", ""]);
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const [guessNumber, setGuessNumber] = useState(0);
+	const [showWinningModal, setShowWinningModal] = useState(false);
+	const [isGameComplete, setIsGameComplete] = useState(false);
 
 	useEffect(() => {
 		const getData = async () => {
@@ -70,6 +71,14 @@ function App() {
 		// Do check to see if word is the correct amount of letters.
 		// Do check to see if word already exists in api response.
 		// Do check to see if letters are in correct space or in the word elsewhere.
+		if (guesses[guessNumber].length !== todaysPokemonLetters) {
+			setErrorMessage("Not enough characters entered!");
+			return;
+		}
+		if (guesses[guessNumber] === todaysPokemon) {
+			setShowWinningModal(true);
+			setIsGameComplete(true);
+		}
 
 		setGuessNumber(guessNumber + 1);
 		document?.getElementById("guess_input")?.focus();
@@ -83,56 +92,81 @@ function App() {
 		// guesses[guessNumber] = event.target.value;
 	};
 
+	const handleClose = () => {
+		setShowWinningModal(false);
+	};
+
 	return (
-		<Layout className="App d-flex">
-			<Header className="wordle__header">
-				<div className="wordle__title">Pokemon Wordle</div>
-				{/* <div>Today's pokemon is... {myData[todaysPokemon]?.name}</div> */}
-			</Header>
-			<Content className="wordle__board">
-				<div>
-					{[...Array(6)].map((value: undefined, index: number) => (
-						<GameRow
-							// todaysWord="test"
-							// guess="test"
-							todaysPokemonLetters={todaysPokemonLetters}
-							guessValue={guesses[index]}
-						/>
-					))}
-				</div>
-				{/* <div>
+		<>
+			{showWinningModal}
+			<Modal
+				title="Congratulations!!!"
+				visible={showWinningModal}
+				onOk={handleClose}
+				onCancel={handleClose}
+			>
+				<p>
+					You're a winner! It took you {guessNumber} guess
+					{guessNumber > 1 ? <>es</> : null} today!
+				</p>
+			</Modal>
+
+			<Layout className="App d-flex">
+				<Header className="wordle__header">
+					<div className="wordle__title">Pokemon Wordle</div>
+					{/* <div>Today's pokemon is... {myData[todaysPokemon]?.name}</div> */}
+				</Header>
+				<Content className="wordle__board">
+					<div>
+						{[...Array(6)].map((value: undefined, index: number) => (
+							<GameRow
+								// todaysWord="test"
+								// guess="test"
+								todaysPokemonLetters={todaysPokemonLetters}
+								guessValue={guesses[index]}
+							/>
+						))}
+					</div>
+					{/* <div>
           {for(let i = 0; i < maxGuesses; i++) {
             <GameRow />
           }}
         </div> */}
 
-				{/* TODO: Render 6 rows - Make   */}
-				<div>
+					{/* TODO: Render 6 rows - Make   */}
+					{/* <div>
 					{myData[todaysPokemon] && (
 						<div className="">
 							<GameRow todaysWord={myData[todaysPokemon]?.name} guess={guess} />
 						</div>
 					)}
-				</div>
-			</Content>
+				</div> */}
+				</Content>
 
-			<Footer>
-				<Input.Group compact>
-					<Input
-						id="guess_input"
-						style={{ width: "calc(100% - 200px)" }}
-						placeholder="Guess a pokemon..."
-						onChange={handleChange}
-						value={guesses[guessNumber]}
-						maxLength={todaysPokemonLetters}
-						className="wordle__input"
-					/>
-					<Button type="primary" onClick={handleSubmit}>
-						Submit Guess
-					</Button>
-				</Input.Group>
-			</Footer>
-		</Layout>
+				<Footer>
+					<Input.Group compact>
+						<Input
+							id="guess_input"
+							style={{ width: "calc(100% - 200px)" }}
+							placeholder="Guess a pokemon..."
+							onChange={handleChange}
+							value={guesses[guessNumber]}
+							maxLength={todaysPokemonLetters}
+							className="wordle__input"
+							disabled={isGameComplete}
+						/>
+						<Button
+							type="primary"
+							onClick={handleSubmit}
+							disabled={isGameComplete}
+						>
+							Submit Guess
+						</Button>
+					</Input.Group>
+					{errorMessage && <div className="error_message ">{errorMessage}</div>}
+				</Footer>
+			</Layout>
+		</>
 	);
 }
 
